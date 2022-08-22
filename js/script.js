4dd4ab19af7c1099e419713a2bf776c5073a9fbc
Md. Shehab Chowdhury <shehab15-11396@diu.edu.bd>
2022-08-23 04:10:09 +0600

    deleting file

--- js/script.js
+++ js/script.js
@@ -28,3 +28,37 @@ function addDisableClass(btn) {
   }
   count++;
 }
+
+/////--------------------------------calculation part
+function validation(value, name) {
+  if (isNaN(value)) {
+    alert("Please Enter A Number..");
+    let cost = document.getElementById(name);
+    cost.value = "";
+  } else {
+    return 1;
+  }
+}
+
+function cost() {
+  return parseInt(document.getElementById("cost").value);
+}
+
+function totalCost(btn) {
+  let fcost = cost();
+  if (validation(fcost, "cost") == 1) {
+    //alert(cost * count);
+    let expense = document.getElementById("expense");
+    expense.innerText = fcost * count;
+  }
+}
+
+function totalCalculation() {
+  let manager = parseInt(document.getElementById("manager").value);
+  let coach = parseInt(document.getElementById("coach").value);
+
+  if (validation(manager, "manager") + validation(coach, "coach") == 2) {
+    let total = document.getElementById("total");
+    total.innerText = manager + coach + cost() * count;
+  }
+}
